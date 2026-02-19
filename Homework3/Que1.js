05a4d222ca5d441ca06e2797003706161999fab5
riyapawar8003 <168684973+riyapawar8003@users.noreply.github.com>
2026-02-18 19:57:45 -0500

    Homework3 commit 2 que

---
+++ Homework3/Que1.js
@@ -0,0 +1,13 @@
+function reverseNumber(x) {
+    let reversed = 0;
+  
+    while (x > 0) {
+      let digit = x % 10;         
+      reversed = reversed * 10 + digit; 
+      x = Math.floor(x / 10);    
+    }
+  
+    return reversed;
+  }
+  
+  console.log(reverseNumber(32243));
